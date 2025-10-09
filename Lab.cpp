4576553f1cc3546ec0f59f5ff3d11064dcd7bdd1
igavd03-plus <igavd03@gmail.com>
2025-10-09 18:49:49 +0300

    first steps

---
+++ Lab.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main () {
+    # ## #
+    # !? #  
+    * ## #
+
+
+
+
+
+
+    return 0;
+}
